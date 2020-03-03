3a1421cd339b12edfd03a7788fad62854a6fb333
Haengbong <kim11560@naver.com>
2020-03-03 23:36:29 +0900

    added virtualenv

---
+++ venv_laptop/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\OneDrive\Python\Django\prography_6th_django_kimhyein\venv_laptop\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
